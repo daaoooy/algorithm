b4676e2044733175859cb9afea7c68762506e85e
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-02-04 23:33:58 +0900

    [Silver V] Title: 거스름돈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,12 @@
+change = int(input())
+count = 0
+
+while change >= 0:
+    if change % 5 == 0:
+        count += (change // 5)
+        print(count)
+        break
+    change -= 2
+    count += 1
+else:
+    print(-1)
