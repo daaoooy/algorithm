d44f501211c4030bb2f998f05a9a68a68586af87
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-03-24 18:59:36 +0900

    [Silver III] Title: 1로 만들기, Time: 204 ms, Memory: 41400 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim();
+
+let N = Number(input);
+
+let dp = [];
+
+dp[0] = -1;
+dp[1] = 0;
+dp[2] = 1;
+dp[3] = 1;
+
+f(N);
+
+console.log(dp[N]);
+
+function f(n) {
+  for (let i = 4; i <= n; i++) {
+    let divide2Case = 0;
+    let divide3Case = 0;
+    let subtraction1Case = 0;
+
+    if (i % 3 === 0 && i % 2 === 0) {
+      divide2Case = dp[i / 2];
+      divide3Case = dp[i / 3];
+
+      dp[i] = divide3Case >= divide2Case ? 1 + divide2Case : 1 + divide3Case;
+    } else if (i % 3 === 0) {
+      divide3Case = dp[i / 3];
+      subtraction1Case = dp[i - 1];
+      dp[i] =
+        divide3Case >= subtraction1Case
+          ? 1 + subtraction1Case
+          : 1 + divide3Case;
+    } else if (i % 2 === 0) {
+      divide2Case = dp[i / 2];
+      subtraction1Case = dp[i - 1];
+      dp[i] =
+        divide2Case >= subtraction1Case
+          ? 1 + subtraction1Case
+          : 1 + divide2Case;
+    } else {
+      subtraction1Case = dp[i - 1];
+      dp[i] = 1 + subtraction1Case;
+    }
+  }
+}
