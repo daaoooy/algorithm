5c0cddee51014c83ad6039e2150c05e3710a7834
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:26:36 +0900

    [Bronze II] Title: JOI와 IOI, Time: 128 ms, Memory: 14712 KB -BaekjoonHub

---
+++ 백준/Bronze/5586. JOI와 IOI/JOI와 IOI.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		
+		int joi = 0;
+		int ioi = 0;
+		
+		for (int i = 0; i < str.length()-2; i++) {
+			if ((str.substring(i, i+3)).equals("JOI")) {
+				joi ++;
+			}
+			else if ((str.substring(i, i+3)).equals("IOI")){
+				ioi ++;
+			}
+		}
+
+		bw.write(Integer.toString(joi));
+		bw.newLine();
+		bw.write(Integer.toString(ioi));
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
+
