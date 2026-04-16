24b9e0368b96c435acd0a5beb6fa1d6f5bb344b7
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:39:30 +0900

    [Bronze V] Title: Hello Judge, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= n; i++) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("Hello World, Judge ").append(Integer.toString(i)).append("!");
+			bw.write(sb.toString());
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
+
