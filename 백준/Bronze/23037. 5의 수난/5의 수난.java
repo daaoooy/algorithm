c44fac7047eb73ebae66f4c3f8fa3256be49fb2e
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:14:06 +0900

    [Bronze II] Title: 5의 수난, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/23037. 5의 수난/5의 수난.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String [] input_str = (br.readLine()).split("");
+		int [] input_int = new int [input_str.length];
+		
+		for (int i = 0; i < input_str.length; i++) {
+			input_int[i] = Integer.parseInt(input_str[i]);
+		}
+		
+		int result = 0;
+		
+		for (int i = 0; i < input_int.length; i++) {
+			int mul = 1;
+			for (int j = 0; j < 5; j++) {
+				mul *= input_int[i];
+			}
+			result += mul;
+		}
+		
+		bw.write(Integer.toString(result));
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+}
+
