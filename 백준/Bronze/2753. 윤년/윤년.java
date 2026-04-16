bd9aa943cd8712ac4238fb19bdbd76a2f24f40ab
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:48:56 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		if ((n % 4 == 0) && (n % 100 != 0)) System.out.println("1");
+		else if (n % 400 == 0) System.out.println("1");
+		else System.out.println("0");
+		
+		input.close();
+	}
+
+}
