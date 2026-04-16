57f805b2bada3c0ff0bd839d7e555218aaf52f64
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:01:16 +0900

    [Silver IV] Title: 빙고, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -7,23 +7,22 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int[][] me = new int[5][5];
-		HashMap<Integer, int[]> location = new HashMap<>();
+		HashMap<Integer, int[]> board = new HashMap<>();
 
 		for (int i = 0; i < 5; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 5; j++) {
-				me[i][j] = Integer.parseInt(st.nextToken());
-				location.put(me[i][j], new int[]{i, j});
+				int num = Integer.parseInt(st.nextToken());
+				me[i][j] = num;
+				board.put(num, new int[]{i, j});
 			}
 		}
 
-		int result = 0;
-
+		int cnt = 0;
 		int[] row = new int[5]; 
 		int[] col = new int[5]; 
 		int d1 = 0;
 		int d2 = 0;
-
 		int bingo = 0;
 		int finish = 0;
 
@@ -31,30 +30,20 @@ public class Main {
 			for (int i = 0; i < 5; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				while (st.hasMoreTokens()) {
-					result ++;
-
+					cnt++;
 					int mc = Integer.parseInt(st.nextToken());
-					if (location.containsKey(mc)) {
-						int x = location.get(mc)[0];
-						int y = location.get(mc)[1];
+					if (board.containsKey(mc)) {
+						int x = board.get(mc)[0];
+						int y = board.get(mc)[1];
 						me[x][y] = 0;
-
-						row[x] ++;
-						col[y] ++;
-
-						if (row[x] == 5) bingo ++;
-						if (col[y] == 5) bingo ++;
-						if (x == y) {
-							d1 ++;
-							if (d1 == 5) bingo ++;
-						}
-						if (x + y == 4) {
-							d2 ++;
-							if (d2 == 5) bingo ++;
-						}
+						
+						if (++row[x] == 5) bingo++;
+						if (++col[y] == 5) bingo++;
+						if (x == y && ++d1 == 5) bingo++;
+						if (x + y == 4 && ++d2 == 5) bingo++;
 
 						if (bingo >= 3) {
-							bw.write(Integer.toString(result));
+							bw.write(Integer.toString(cnt));
 							finish = 1;
 							break;
 						}
