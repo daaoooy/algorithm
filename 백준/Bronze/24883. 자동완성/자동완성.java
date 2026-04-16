79caecb9724a093d5f179fe0c63bc250b72d1a53
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:17:09 +0900

    [Bronze V] Title: 자동완성, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input = br.readLine();
+		
+		if (input.equals("N") || input.equals("n")) {
+			bw.write("Naver D2");
+		}
+		else {
+			bw.write("Naver Whale");
+		}
+		bw.flush();
+		bw.close();
+
+	}
+
+}
+
