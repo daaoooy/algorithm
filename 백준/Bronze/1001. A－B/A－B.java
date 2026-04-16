2a945b8848612c6e7ef0bbbbcddeced102f9ee08
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 16:24:50 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		int result = a - b;
+		System.out.println(result);
+		
+		input.close();
+	}
+
+}
