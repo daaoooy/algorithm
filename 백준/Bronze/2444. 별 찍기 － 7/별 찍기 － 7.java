4f4b46d1dea62e15b73e34ff2f23233d428677ee
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:22:41 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 452 ms, Memory: 20780 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -9,25 +9,33 @@ public class Main {
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
 		int n = Integer.parseInt(br.readLine());
-
+		
 		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n-1; j++) bw.write(" ");
-			for (int j = 0; j < 2*i+1; j++) bw.write("*");		
-			bw.newLine();
+			for (int j = i; j < n-1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2*i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-
+		
 		for (int i = n-1; i >= 1; i--) {
-			for (int j = n-1; j >= i; j--) bw.write(" ");
-			for (int j = 2*i-1; j > 0; j--) bw.write("*");	
-			bw.newLine();
+			for (int j = n-1; j >= i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 2*i-1; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 
 		bw.flush();
 		bw.close();
 
 	}
-
+	
 }
 
