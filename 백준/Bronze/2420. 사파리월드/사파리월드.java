450f993c597bbb25cd6e6436dff277a4d28e3183
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:54:16 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		long n = input.nextLong();
+		long m = input.nextLong();
+		
+		System.out.println(Math.abs(n-m));
+		
+		input.close();
+	}
+
+}
