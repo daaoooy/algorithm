fb65522763e8548cd193918fb7dba09e3d014753
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:21:44 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 140 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			for (int j = n-1; j > i; j--) bw.write(" ");
+			if (i == 0) bw.write("*");
+			else if (i == n-1) 
+				for (int j = 0; j < 2*n-1; j++) bw.write("*");
+			else {
+				for (int j = n-1-i; j <= n-1 + i; j++) {
+					if (j == n-1-i || j ==  n-1+i) bw.write("*");
+					else bw.write(" ");
+				}
+			}
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
+
