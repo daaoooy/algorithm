1eedde419168b2aa6f5bf501847657052f0170b6
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:06:18 +0900

    [Silver V] Title: 단어 나누기, Time: 156 ms, Memory: 17144 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -3,6 +3,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {        
@@ -11,28 +14,25 @@ public class Main {
         
         String input = br.readLine();
         StringBuilder word = new StringBuilder(input);
-        String result = null;
+        List<String> list = new ArrayList<>();
         
-        for (int i = 1; i < word.length()-1; i++) {
-        	for (int j = i+1; j < word.length(); j++) {
-        		StringBuilder first = new StringBuilder(word.substring(0, i));
-        		StringBuilder second = new StringBuilder(word.substring(i, j));
-        		StringBuilder third = new StringBuilder(word.substring(j));
+        for (int i = 2; i < word.length(); i++) {
+        	for (int j = 1; j < i; j++) {
+        		StringBuilder first = new StringBuilder(word.substring(0, j));
+        		StringBuilder second = new StringBuilder(word.substring(j, i));
+        		StringBuilder third = new StringBuilder(word.substring(i));
         		
         		first = first.reverse();
         		second = second.reverse();
         		third = third.reverse();
         		
-        		String combined = first.toString() + second.toString() + third.toString();
-        		
-        		
-        		if (result == null || combined.compareTo(result) < 0) {
-        			result = combined;
-        		}
+        		String result = first.toString() + second.toString() + third.toString();
+        		list.add(result);
         	}
         }
         
-        bw.write(result);
+        Collections.sort(list);
+        bw.write(list.get(0));
         bw.flush();
         bw.close();
         br.close();
