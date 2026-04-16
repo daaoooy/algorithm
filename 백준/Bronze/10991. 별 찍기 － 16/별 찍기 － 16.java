eb30273f5fe17d2a4952172f599547ce3d3699aa
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 17:21:52 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 136 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			for (int j = n-1; j > i; j--) bw.write(" ");
+			
+			if (n % 2 == 0) 
+				for (int j = n-i; j <= n + i; j++) {
+					if (i % 2 == 0) {
+						if (j % 2 == 0) bw.write("*");
+						else bw.write(" ");
+					}
+					else {
+						if (j % 2 == 0) bw.write(" ");
+						else bw.write("*");
+					}
+				}
+			else {
+				for (int j = n-1-i; j <= n + i; j++) {
+					if (i % 2 == 0) {
+						if (j % 2 == 0) bw.write("*");
+						else bw.write(" ");
+					}
+					else {
+						if (j % 2 == 0) bw.write(" ");
+						else bw.write("*");
+					}
+				}
+			}
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
+
