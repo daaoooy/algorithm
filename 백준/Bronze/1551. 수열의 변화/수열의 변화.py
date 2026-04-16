751d926d6ab1e7aad109c2bfb8a1894831c5439e
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2026-04-16 20:32:53 +0900

    [Bronze I] Title: 수열의 변화, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split(" "))
+A = list(map(int, input().split(",")))
+
+for _ in range(K):
+    B = []
+    for i in range(1, len(A)):
+        B.append(A[i] - A[i-1])
+    A = B
+
+print(",".join(map(str, A)))
